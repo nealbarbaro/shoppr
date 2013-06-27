14b3373dae26f9227fa5f93aa943c3da82232c94
Neal Barbaro <nealbarbaro@gmail.com>
2013-06-27 16:15:23 -0700

    start building parse_gilt_function

--- shoppr.rb
+++ shoppr.rb
@@ -2,3 +2,14 @@ require 'pry'
 require 'httparty'
 
 # bd7150e5387c083154bc8b35a9719362
+
+results = HTTParty.get("https://api.gilt.com/v1/products?q=jeans&store=women&apikey=bd7150e5387c083154bc8b35a9719362)
+
+def parse_gilt_results(results)
+  results["products"].each do |product|
+
+
+
+  end
+
+end
